5ddc861797053645aef807962431657f4daf1a5d
samslezek <sdotslezek@gmail.com>
2019-12-29 13:54:57 -0800

    fixed .gitignore

---
+++ client/build/precache-manifest.01f80c2460cf06dc1b88cd552872d088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa268370d47f55c2522f079e87eb6f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e969719b7e2961e2930",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0db7f173578cbe33f01c",
+    "url": "/static/css/main.a9135a62.chunk.css"
+  },
+  {
+    "revision": "0e969719b7e2961e2930",
+    "url": "/static/js/2.e7ef46f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e7ef46f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0db7f173578cbe33f01c",
+    "url": "/static/js/main.8d212df1.chunk.js"
+  },
+  {
+    "revision": "8f681e8a26263e7285da",
+    "url": "/static/js/runtime-main.b91df706.js"
+  }
+]);
