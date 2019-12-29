41c379bd709a5cdd5bc1f79b08b2213633142273
samslezek <sdotslezek@gmail.com>
2019-12-29 10:15:15 -0800

    tried to look in a diff directory once again

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var User = require("../models/user");
 var router  = express.Router();
 
 // Serve static files from the React app
-router.use(express.static(path.join(__dirname, '../client/build')));
+router.use(express.static(path.join(__dirname, '/../client/build')));
 
 router.get("/react", function(req, res){
 	console.log('got this call')
