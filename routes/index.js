8b7f85f48a6522b393d9baca2e9dbef96b0bc3ea
samslezek <sdotslezek@gmail.com>
2019-12-29 08:33:54 -0800

    changed to /reactapp

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.post("/react", function(req, res){
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/reactapp', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
