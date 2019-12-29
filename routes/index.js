aea8c1b4dd8facac26ae15ef02e89422fb36a569
samslezek <sdotslezek@gmail.com>
2019-12-29 08:31:56 -0800

    added the catchall url handler

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,10 @@ router.post("/react", function(req, res){
     });
 });
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 module.exports = router;
